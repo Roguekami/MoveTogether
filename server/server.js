974803446c2f26e87bc9caf619642f57a060ebd5
Your Name <youremail@example.com>
2026-05-11 01:02:12 +0100

    Remove express-mongo-sanitize as it is incompatible with Express 5

--- server/server.js
+++ server/server.js
@@ -20,7 +20,6 @@ const io = new Server(server, {
 });
 
 const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
@@ -32,7 +31,6 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // Allow images to load from other domains like Cloudinary
 app.use(cors());
 app.use(express.json());
-app.use(mongoSanitize());
 app.use(xss());
 
 // Global Rate Limiting
